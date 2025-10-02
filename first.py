6f46c8fad45d52ce3e77bbd42c9c0902cec00b37
petulos <135587810+petulos@users.noreply.github.com>
2025-10-02 08:55:52 +0000

    prvni ukol na cviceni

---
+++ first.py
@@ -0,0 +1,14 @@
+
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        print( f"Číslo {cislo} je sude")
+        return f"Číslo {cislo} je sude"
+    else:
+        print( f"Číslo {cislo} je liche")
+        return f"Číslo {cislo} je liche"
+        
+
+
+if __name__ == "__main__":
+    sudy_nebo_lichy(5)
+    sudy_nebo_lichy(1000000)
