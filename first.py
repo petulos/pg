8dbc887daa5fd73b9544fba5eddec199268c190f
Petr Valíček <135587810+petulos@users.noreply.github.com>
2025-10-03 11:40:32 +0200

    Update first.py

--- first.py
+++ first.py
@@ -1,10 +1,10 @@
 
 def sudy_nebo_lichy(cislo):
     if cislo % 2 == 0:
-
+        print(f"Číslo {cislo} je sudé")
         return f"Číslo {cislo} je sudé"
     else:
-
+        f"Číslo {cislo} je liché"
         return f"Číslo {cislo} je liché"
         
 
